dc61c80dfbfebf9508e8fd593b02ed2c6f456a7b
ljc <2432402366@qq.com>
2023-07-01 18:27:05 +0800

    linux文件系统

--- _includes/scripts/article.js
+++ _includes/scripts/article.js
@@ -1,22 +1,37 @@
-(function() {
+(function () {
   var SOURCES = window.TEXT_VARIABLES.sources;
-  window.Lazyload.js(SOURCES.jquery, function() {
-    $(function() {
-      var $this ,$scroll;
+  window.Lazyload.js(SOURCES.jquery, function () {
+    $(function () {
+      var $this, $scroll;
       var $articleContent = $('.js-article-content');
       var hasSidebar = $('.js-page-root').hasClass('layout--page--sidebar');
       var scroll = hasSidebar ? '.js-page-main' : 'html, body';
       $scroll = $(scroll);
 
-      $articleContent.find('.highlight').each(function() {
+      $articleContent.find('.highlight').each(function () {
         $this = $(this);
+        let $code = $this.find('code')
+        console.log('22222', $code)
+        // $code.append($('<button class="Clipboard_btn" data-clipboard-target=".highlight">复制/Copy</button>'));
+        // new ClipboardJS('.Clipboard_btn')
+        // if ($code.find('.Clipboard_btn').length == 0) {
+        $code.append($('<button class="Clipboard_btn" data-clipboard-target=".highlight">复制/Copy</button>'));
+        new ClipboardJS('.Clipboard_btn')
+        // }
         $this.attr('data-lang', $this.find('code').attr('data-lang'));
       });
-      $articleContent.find('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').each(function() {
+      // $articleContent.find('code').each(function () {
+
+      //   $this = $(this);
+      //   console.log('调试=====', $this.find('.Clipboard_btn').length)
+      //   $this.append($('<button class="Clipboard_btn" data-clipboard-target=".highlight">复制/Copy</button>'));
+      //   new clipboardJS('.Clipboard_btn')
+      // });
+      $articleContent.find('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').each(function () {
         $this = $(this);
         $this.append($('<a class="anchor d-print-none" aria-hidden="true"></a>').html('<i class="fas fa-anchor"></i>'));
       });
-      $articleContent.on('click', '.anchor', function() {
+      $articleContent.on('click', '.anchor', function () {
         $scroll.scrollToAnchor('#' + $(this).parent().attr('id'), 400);
       });
     });
--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -2,6 +2,13 @@
   var $root = document.getElementsByClassName('root')[0];
   if (window.hasEvent('touchstart')) {
     $root.dataset.isTouch = true;
-    document.addEventListener('touchstart', function(){}, false);
+    document.addEventListener('touchstart', function () { }, false);
   }
+  //测试在全局引入需要的CDN库
+  let body = document.getElementsByTagName('body')[0]
+  let script = document.createElement('script')
+
+  var Clipboard_CDN = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js'
+  script.setAttribute('src', Clipboard_CDN)
+  $root.appendChild(script)
 })();
